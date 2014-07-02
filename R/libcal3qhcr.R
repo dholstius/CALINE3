628995763e78750d85d3c8eb1ea6b691cec17864
David Holstius <david.holstius@gmail.com>
2014-07-02 11:30:14 -0700

    Add CALINE3_RECEPTOR_TOTALS

---
+++ R/libcal3qhcr.R
@@ -0,0 +1,111 @@
+#' CALINE3_RECEPTOR_TOTALS
+#'
+#' For each receptor, predict cumulative concentrations (contributed by all links)
+#' under a number of different meteorological conditions.
+#'
+#' All coordinates are in meters unless otherwise specified.
+#'
+#' @param XR x-coordinates of the receptors
+#' @param YR y-coordinates of the receptors
+#' @param ZR z-coordinates of the receptors (height above ground level, usually 1.8m)
+#' @param XL1 starting x-coordinates of the links
+#' @param YL1 starting y-coordinates of the links
+#' @param XL2 ending x-coordinates of the links
+#' @param YL2 ending y-coordinates of the links
+#' @param WL widths of the links
+#' @param HL heights of the links (above ground level)
+#' @param NTYP link classifications (1=at grade, 2=bridge, 3=fill, 4=depressed)
+#' @param VPHL link-level traffic volumes, in vehicles per hour
+#' @param EFL link-level emission factors, in grams per vehicle-mile per hour
+#' @param U wind speeds, in meters per second (not less than 1.0)
+#' @param BRG wind bearings, in degrees (direction wind is blowing from)
+#' @param CLAS stability classes (1, 2, 3, 4, 5, or 6)
+#' @param MIXH mixing heights, in meters (over 1000 skips mixing height calculations)
+#' @param ATIM averaging time, in minutes (usually 60)
+#' @param Z0 surface roughness, in centimeters
+#' @param VS settling velocity, in cm/sec
+#' @param VD deposition velocity, in cm/sec
+#'
+#' @return NR x NM matrix of concentrations, where NR is the number of receptors
+#'         and NM is the number of meteorological conditions
+#'
+#' @useDynLib CALINE3
+#' @rdname CALINE3
+#' @export
+CALINE3_RECEPTOR_TOTALS <- function(
+  XR, YR, ZR,
+  XL1, YL1, XL2, YL2, WL, HL, TYP, VPHL, EFL,
+  UM, BRGM, CLASM, MIXHM,
+  ATIM, Z0, VS, VD
+) {
+
+  # Receptor specifications
+  NR <- as.integer(length(XR))
+  stopifnot(all.equal(NR, length(YR), length(ZR)))
+  if(any(is.na(c(XR, YR, ZR))))
+    stop("Receptor coordinates cannot include NA values.")
+
+  # Link specifications
+  NL <- as.integer(length(XL1))
+  stopifnot(all.equal(NL, length(YL1), length(XL2), length(YL2),
+                      length(WL), length(HL), length(NTYP), length(VPHL), length(EFL)))
+  stopifnot(lapply(list(XR, YR, ZR), is.numeric) == TRUE)
+  stopifnot(lapply(list(XL1, YL1, XL2, YL2, WL, HL, NTYP, VPHL, EFL), is.numeric) == TRUE)
+  stopifnot(lapply(list(UM, BRGM, CLASM, MIXHM), is.numeric) == TRUE)
+  if(any(is.na(c(XL1, YL1, XL2, YL2))))
+    stop("Link coordinates cannot include NA values.")
+  if(any(is.na(WL)))
+    stop("Link widths cannot include NA values.")
+  if(any(is.na(HL)))
+    stop("Link heights cannot include NA values.")
+  if(any(is.na(TYP)))
+    stop("Link classifications cannot include NA values.")
+  if(any(is.na(VPHL)))
+    stop("Link flows cannot include NA values.")
+  if(any(is.na(EFL)))
+    stop("Link emission factors cannot include NA values.")
+  if(is.character(TYP)) {
+    # Convert type classifications to integers.
+    # (because you can't pass characters to .Fortran() with DUP = FALSE )
+    clas.lookup <- list(AG=0, BR=1, FL=2, DP=3,
+                        `At Grade`=0, `Bridge`=1, `Fill`=2, `Depressed`=3)
+    NTYP <- as.integer(clas.lookup[TYP])
+  } else if(is.integer(TYP)) {
+    NTYP <- TYP
+  } else {
+    stop('TYP argument must be character or integer')
+  }
+
+  # Meteorology specifications.
+  NM <- as.integer(length(UM))
+  stopifnot(all.equal(NM, length(BRGM), length(CLASM), length(MIXHM)))
+  if(any(is.na(UM)))
+    stop("Wind speeds cannot include NA values. See ?read.ISC for more.")
+  if(any(is.na(BRGM)) || any(BRGM < 0) || any(BRGM > 360))
+    stop("Wind bearings must be between 0 and 360 degrees, and cannot include NA values. See ?read.ISC for more.")
+  if(any(is.na(CLASM)))
+    stop("Stability classes must not include NA values. See ?read.ISC for more.")
+  if(any(CLASM > 6)) {
+    warning("Replacing stability class 7 with class 6.")
+    CLASM <- pmin(CLASM, 6)
+  }
+  if(any(MIXHM < 0))
+    stop("Mixing heights must not be negative")
+  if(any(is.na(MIXHM)))
+    stop("Mixing heights cannot include NA values.")
+
+  # Call native code, using array C for results
+  rcp_args <- list(XR, YR, ZR)
+  lnk_args <- list(XL1, YL1, XL2, YL2, WL, HL, NTYP, VPHL, EFL)
+  met_args <- list(UM, BRGM, CLASM, MIXHM)
+  aux_args <- list(ATIM, Z0, VS, VD)
+  shape <- c(NR, NL)
+  C <- as.single(array(0.0, dim=shape))
+  retval <- do.call(
+    ".Fortran",
+    c("CALINE3_RECEPTOR_TOTALS", NR, rcp_args, NL, lnk_args, NM, met_args, aux_args, C=C),
+    PACKAGE = "CALINE3"
+  )
+  return(array(as.double(retval$C), dim=shape))
+
+}
